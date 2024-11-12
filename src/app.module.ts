8f85fcafb61f26250cfc2652409ca6c96ac87a77
VladHolobyn <125756054+VladHolobyn@users.noreply.github.com>
2024-11-12 22:48:16 +0200

    RP-3: Create controllers (#4)
    * added auth controller
    
    * added groups controller
    
    * added slots controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { GroupsModule } from './groups/groups.module';
+import { SlotsModule } from './slots/slots.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, GroupsModule, SlotsModule],
   controllers: [AppController],
   providers: [AppService],
 })
